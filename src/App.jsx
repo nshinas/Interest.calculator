151790d7ae6d8da1cfcd66c2f5e843e41d48aacb
unknown <tt.shinas06@gmail.com>
2024-10-17 10:46:59 +0530

    interestapp

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,64 @@ function App() {
   const [rate,setRate] = useState(0)
   const [year,setYear] = useState(0)
 
+  const [invalidPrinciple,setInvalidPrinciple] =useState(false)
+  const [invalidRate,setInvalidRate] =useState(false)
+  const [invalidYear,setInvalidYear] =useState(false)
+
+  
+
   const validateInputs = (inputTag)=>{
     console.log(typeof inputTag);
     const {name,value} = inputTag
     console.log(name,value);
     console.log(!!value.match(/^[0-9]*.?[0-9]+$/));
     console.log(!!value.match(/^\d*\.?\d+$/));
+    if(name=="principle"){
+      setPrinciple(value)
+      if(!!value.match(/^\d*\.?\d+$/)){
+        setInvalidPrinciple(false)
+      }else{
+        setInvalidPrinciple(true)
+      }
+    }else if(name=="rate"){
+      setRate(value)
+      if(!!value.match(/^\d*\.?\d+$/)){
+        setInvalidRate(false)
+      }else{
+        setInvalidRate(true)
+      }
+    }else if(name=="year"){
+      setYear(value)
+      if(!!value.match(/^\d*\.?\d+$/)){
+        setInvalidYear(false)
+      }else{
+        setInvalidYear(true)
+      }
+    }
+  
+    
+    
+  }
 
+  const handleCalculate = (e) =>{
+    e.preventDefault()
+    console.log("inside handleCalculate");
+    if(priciple && rate && year){
+      setInterst(priciple*rate*year/100)
+    }else{
+      alert("Please fill the form completely!!!")
+    }
     
-    
+  }
+
+  const handleReset = () =>{
+    setInterst(0)
+    setPrinciple(0)
+    setRate(0)
+    setYear(0)
+    setInvalidPrinciple(false)
+    setInvalidRate(false)
+    setInvalidYear(false)
   }
 
   return (
@@ -29,25 +78,37 @@ function App() {
           <h3>Simple Interest Calculator</h3>
           <p>Calculate your simple interest Easily!</p>
           <div className='bg-warning p-3 text-light text-center rounded'>
-            <h1>₹ 1500</h1>
+            <h1>₹ {interest}</h1>
             <p className='fa-bolder'>Total Simple Interest</p>
           </div>
           <form className='mt-5'>
             {/* principle */}
             <div className='mb-3'>
-            <TextField name='principle' onChange={(e)=>validateInputs(e.target)} className='w-100' id="outlined-priciple" label="priciple amount ₹" variant="outlined" />
+            <TextField value={priciple ||""} name='principle' onChange={(e)=>validateInputs(e.target)} className='w-100' id="outlined-priciple" label="priciple amount ₹" variant="outlined" />
             </div>
+            {/* invalid principle amount */}
+            { invalidPrinciple && <div className='mb-3 text-danger fw-bolder'>
+              Invalid Principle Amount!!!
+            </div>}
             {/*Rate  */}
             <div className='mb-3'>
-            <TextField className='w-100' id="outlined-rate" label="Rate of interest(%)" variant="outlined" />
+            <TextField value={rate ||""} name='rate' onChange={(e)=>validateInputs(e.target)} className='w-100' id="outlined-rate" label="Rate of interest(%)" variant="outlined" />
             </div>
+            {/* invalid Rate */}
+            { invalidRate && <div className='mb-3 text-danger fw-bolder'>
+              Invalid Rate!!!
+            </div>}
             {/* Year */}
             <div className='mb-3'>
-            <TextField className='w-100' id="outlined-year" label="Time period (yr)" variant="outlined" />
+            <TextField value={year ||""} name='year' onChange={(e)=>validateInputs(e.target)} className='w-100' id="outlined-year" label="Time period (yr)" variant="outlined" />
             </div>
+            {/* invalid year */}
+            { invalidYear && <div className='mb-3 text-danger fw-bolder'>
+              Invalid Year!!!
+            </div>}
             <Stack direction="row" spacing={2}>
-            <Button variant="contained" style={{width:'50%',height:'60px'}} className='bg-dark'>Calculate</Button>
-            <Button variant="outlined" style={{width:'50%',height:'60px'}} className='border border-dark text-dark'>Reset</Button>
+            <Button type='submit' onClick={handleCalculate} disabled={invalidPrinciple || invalidRate || invalidYear} variant="contained" style={{width:'50%',height:'60px'}} className='bg-dark'>Calculate</Button>
+            <Button onClick={handleReset} variant="outlined" style={{width:'50%',height:'60px'}} className='border border-dark text-dark'>Reset</Button>
              </Stack>
           </form>
         </div>
